a24bc4d926001135c4631a9f5c55e81f560122df
Falcao5 <marte.vfalcone@gmail.com>
2016-11-14 11:00:14 +0100

    unione header vale e vins

--- stringhe.h
+++ stringhe.h
@@ -2,11 +2,20 @@
 #ifndef STRINGHE_H
 #define STRINGHE_H
 typedef char* stringa;
+typedef struct{     // Struttura dati
+    char cOcc;      // Carattere piu ricorrente
+    int numOcc;     // Numero di volte che questo carattere compare nella sequqnza
+}Container;
 #endif // STRINGHE_H
 
+void leggiStringaArray(char s[]);
 char* creaStringa();
-void stampaStringa(char *s);
+void stampaStringaArray(char s[]);
+void stampaStringaPuntatore(char *s);
 char* copiaStringa(stringa ss);
 int confrontaStringhe(stringa s1,stringa s2);
 char* concatenaStringhe(stringa s1, stringa s2);
+Container letteraPiuRicorrente(char s[]);
+void stringheVins();
+void stringheVale();
 void stringhe();
