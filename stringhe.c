4184d527e9559908dccc849aa7b9086e874b54b9
Falcao5 <marte.vfalcone@gmail.com>
2016-11-14 10:58:29 +0100

    unione di esercizi vale e vins

--- stringhe.c
+++ stringhe.c
@@ -21,6 +21,17 @@
     str="Questa e' una Prova";  --> SBAGLIATO
 */
 
+void leggiStringaArray(char s[]){
+    int i;
+
+    printf("Inserire %d caratteri!\n",N);
+
+    for(i=0;i<N-1;i++){
+        scanf("%c", &s[i]);
+    }
+
+}
+
 char* creaStringa(){
     stringa s=(stringa)malloc(sizeof(char));
     printf("Inserisci una stringa qualsiasi:\n");
@@ -28,7 +39,15 @@ char* creaStringa(){
     return s;
 }
 
-void stampaStringa(stringa s){
+void stampaStringaArray(char s[]){
+    int i;
+
+    printf("La stringa inserita e':\n");
+    for(i=0;s[i]!='\0';i++)
+        printf("%c",s[i]);
+}
+
+void stampaStringaPuntatore(stringa s){
     printf("%s\n",s);
 }
 
@@ -46,7 +65,40 @@ char* concatenaStringhe(stringa s1, stringa s2){
     return strcat(s1,s2);
 }
 
-void stringhe(){
+Container letteraPiuRicorrente(char s[]){
+    Container result;   // Struttura dati
+
+    int i;              // Contatori
+    int j;
+
+    char caratterePiuRicorrente;
+    int numeroOccorrenze;
+
+    numeroOccorrenze=1;             // Inizializzazione variabili
+    caratterePiuRicorrente = s[0];
+
+    int temp;      // Variabile temporanea per salvare il numero di ricorrenze di una lettera in un array
+
+    for(i=0;s[i]!='\0';i++){           //Per ogni elemento del vettore
+        temp=1;
+
+        for(j=i+1;s[j]!='\0';j++)     // Per ogni elemento nella parte successiva del vettore
+            if(s[i]==s[j])     //Se il carattere che ho preso in esame e' uguale a uno degli elementi del resto del vettore
+                temp++;
+
+        if(temp>numeroOccorrenze){
+            caratterePiuRicorrente = s[i];
+            numeroOccorrenze = temp;
+        }
+    }
+
+    result.cOcc=caratterePiuRicorrente;
+    result.numOcc=numeroOccorrenze;
+
+    return result;
+}
+
+void stringheVins(){
     int cmp;
     stringa str1,str2;
     str1=creaStringa();
@@ -61,4 +113,29 @@ void stringhe(){
         printf("le stringhe sono diverse\n");
     str2=concatenaStringhe(str1,str2);
     printf("\nstringa risultante = %s",str2);
+
 }
+
+void stringheVale(){
+    char s[N];
+    Container c;
+
+    leggiStringa(s);
+    stampaStringa(s);
+
+    c = letteraPiuRicorrente(s);
+
+    printf("\nLa lettera piu ricorrente nella stringa inserita e': %c\nAppare %d volte.", c.cOcc, c.numOcc);
+
+}
+
+void stringhe(){
+    int n;
+    printf("VALE(1) O VINS(2)?\n");
+    scanf("%d",&n);
+    if(n==1)
+        stringheVale();
+    else
+        stringheVins();
+}
+
