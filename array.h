13401a2fb6880ab3273102c7268fb23c8340b7e6
Falcao5 <marte.vfalcone@gmail.com>
2016-11-16 14:16:02 +0100

    Ci sono riusito! Risolto :D Vins gaiooooooooooooooooooooooooooooooooo

--- array.h
+++ array.h
@@ -9,4 +9,6 @@ int maxElemArray(int *v, int D);
 void pariDispari(int *a, int D);
 void sort(int *a);
 void stampaArray(int *v);
+void potenza(int num, int pot);
+int potenzaRicorsiva(int num, int pot);
 void vet();
