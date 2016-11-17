48e9758abcd0d55e701e240db3cd92d1b56b87b8
vins-87 <vincenzo.dorso@gmail.com>
2016-11-17 16:22:23 +0100

    liste aggiunte

--- liste.c
+++ liste.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "liste.h"
 
+lista* creaLista(){
+    lista *l1=(lista*)malloc(sizeof(lista));
+    l1->prev=NULL;
+    l1->next=NULL;
+    l1->info=0;
+    return l1;
+}
+
 void liste(){
-    printf("Prova Lista\n");
+    lista *l2=(lista*)malloc(sizeof(lista));
+    l2=creaLista();
+    printf("stampa valore nodo: %d",l2->info);
+    free(l2);
 }
--- liste.h
+++ liste.h
@@ -1,6 +1,14 @@
 #ifndef LISTE_H
 #define LISTE_H
 
+typedef struct nodo{
+    struct nodo *prev;
+    struct nodo *next;
+    int info;
+}lista;
+
 #endif // LISTE_H
 
+
+lista* creaLista();
 void liste();
