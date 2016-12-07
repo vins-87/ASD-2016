086740d17ed519aace24102a48e2120f9ca94331
vins-87 <vincenzo.dorso@gmail.com>
2016-12-07 20:08:52 +0100

    rimesso liste di falco

--- liste.c
+++ liste.c
@@ -1,98 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "liste.h"
-#include "array.h"
-
-void stampaLista(plist p){
-    printf("lista: ");
-    if(p!=NULL){
-        while(p->next!=NULL){
-            printf("%d",p->info);
-            p=p->next;
-        }
-    }
-    printf("%d\n",p->info);
-}
-
-int valore_massimo_lista(plist p){
-
-    /* scrivi qui il codice che calcola il valore massimo
-       contenuto in una lista semplicemente concatenata di
-       interi passata in input e lo ritorna come output.
-       Se la lista non ha nessun elemento ritorna zero. */
-
-    int max=0;
-    if(p!=NULL){
-        max=p->info;
-        while(p->next!=NULL){
-            //printf("%d",p->info);
-            if(p->info>max){
-                max=p->info;
-            }
-            p=p->next;
-        }
-        if(p->info>max){
-            max=p->info;
-        }
-    }
-    return max;
-}
-
-int trovaElem(plist *p, int elem){
-    plist temp=*p;
-    while(temp!=NULL){
-        if(temp->info==elem){
-            return 1;
-        }
-        temp=temp->next;
-    }
-    return 0;
-}
-
-void inserisciInCoda(plist *p, int n){
-    plist temp=(plist)malloc(sizeof(elist));
-    temp->info=n;
-    temp->next=NULL;
-    if(*p==NULL){
-        *p=temp;
-    }else{
-        plist temp2=*p;
-        while(temp2->next!=NULL){
-            //scorri lista fino alla fine
-            temp2=temp2->next;
-        }
-        temp2->next=temp;
-    }
-}
-
-int compara_array_lista(int A[], int n, plist p){
-    /* Scrivi qui il codice che conta quanti elementi dell'array A
-       lungo n siano presenti anche nella lista semplicemente
-       concatenata p di interi.
-       L'array A ha tutti elementi distinti, mentre la lista p
-       potrebbe avere elementi duplicati. Se un elemento di A
-       appare duplicato in p, deve essere contato una sola volta.
-       Se la lista e' vuota oppure n e' zero allora ritorna zero.  */
-    int i;
-    int cont=0;
-    plist temp=p;
-    int ret;
-    if(temp!=NULL&&n!=0){
-        plist temp2=NULL;
-        while(temp!=NULL){
-            for(i=0;i<n;i++){
-                if(temp->info==A[i]){
-                    if(ret==0){
-                        inserisciInCoda(&temp2,A[i]);
-                        cont++;
-                    }
-                }
-            }
-            temp=temp->next;
-        }
-    }
-    return cont;
-}
 
 lista* creaLista(){
     lista *l1=(lista*)malloc(sizeof(lista));
@@ -102,29 +10,62 @@ lista* creaLista(){
     return l1;
 }
 
-plist creaElist(){
-    plist p=NULL;
-    printf("inserisci 5 elementi nella lista\n");
+void inserisciInCoda(int x, lista *p){
+    lista *k = (lista*)malloc(sizeof(lista));   // Creo un nuovo nodo
+    k->next = NULL;                             // Inizializzo il nuovo nodo
+    k->info = x;
+
+    while(p->next!=NULL)    // Scorre la lista fino all'ultimo elemento
+        p=p->next;
+
+    k->prev = p;        // Aggiorno il precedente del nuovo nodo
+
+    p->next = k;        // Aggiungo in coda il nuovo nodo
+
+}
+
+void inserisciInTesta(int x, lista *p){
+    lista *k = (lista*)malloc(sizeof(lista));
+    k->prev = NULL;
+    k->info = x;
+    k->next = p;
+    p=k;
+}
+
+/* TEST */
+int testInserimentoInCoda(){
+    int result = 0;
+    int CASES = 4;
     int i;
-    int n;
-    for(i=0;i<5;i++){
-       scanf("%d",&n);
-       inserisciInCoda(&p,n);
+
+    lista *l = (lista*)malloc(sizeof(lista));
+    l = creaLista();
+
+    inserisciInCoda(1,l);
+    inserisciInCoda(2,l);
+    inserisciInCoda(3,l);
+    inserisciInCoda(4,l);
+
+    l=l->next;
+
+    for(i=1;i<CASES+1;i++){
+        if(l->info==i)
+            result++;
+        l=l->next;
     }
-    printf("\n");
-    return p;
+
+    if(result == CASES)
+        return 1;
+    else
+        return 0;
+
 }
 
 void liste(){
-/*    lista *l2=creaLista();
-    printf("stampa valore nodo: %d\n",l2->info);
+    lista *l2=(lista*)malloc(sizeof(lista));
+    l2=creaLista();
+    printf("stampa valore nodo: %d",l2->info);   // Stampa valore primo elemento della lista
+    printf("\nTest di inserimento in coda risulta: %d", testInserimentoInCoda());
+
     free(l2);
-*/    plist p=creaElist();
-/*    stampaLista(p);
-    int max=valore_massimo_lista(p);
-    printf("max elem della lista: %d",max);
-*/  int A[]={2,5,4,6,1};
-    int ret=compara_array_lista(A,5,p);
-    printf("totale di num trovati: %d\n",ret);
-    free(p);
 }
