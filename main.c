f49e5c35c70e8aa3c9a4e22798cf512e99d86eca
Falcao5 <marte.vfalcone@gmail.com>
2016-12-08 14:18:46 +0100

    modificato main

--- main.c
+++ main.c
@@ -8,28 +8,29 @@
 #include "elezioni.h"
 
 int main(){
+int sel;
+    while(sel!=-1){
+        sel=0;
+        printf("Vins ebreo?\n(-1=esci,0=array, 1=matrici, 2=stringhe, 3=strutture, 4=liste, 5=elezioni, 6=alberi binari\n");
+        scanf("%d",&sel);
+        switch(sel){
 
-    int sel=0;
-    printf("Vins ebreo?\n(0=array, 1=matrici, 2=stringhe, 3=strutture, 4=liste, 5=elezioni, 6=alberi binari\n");
-    scanf("%d",&sel);
-    switch(sel){
+            case 0: vet();
+                    break;
+            case 1: matrice();
+                    break;
+            case 2: stringhe();
+                    break;
+            case 3: strutture();
+                    break;
+            case 4: liste();
+                    break;
+            case 5: elezioni();
+                    break;
 
-        case 0: vet();
-                break;
-        case 1: matrice();
-                break;
-        case 2: stringhe();
-                break;
-        case 3: strutture();
-                break;
-        case 4: liste();
-                break;
-        case 5: elezioni();
-                break;
-
-        default: printf("esci");
-                break;
+            default: printf("esci");
+                    break;
+        }
     }
-
     return 0;
 }
