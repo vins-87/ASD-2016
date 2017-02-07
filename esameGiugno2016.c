6b99719475f53354363dd1239e9a858c8cb8feeb
vins-87 <vincenzo.dorso@gmail.com>
2017-02-07 11:43:54 +0100

    finito esercizio verificaDiscendenza

--- esameGiugno2016.c
+++ esameGiugno2016.c
@@ -8,62 +8,32 @@ binario T, e due stringhe S1 e S2 ritorni 1 se il nodo contenente S2 discende da
 altrimenti. Nel caso in cui l’albero T sia vuoto, la stringa S1 non esista in T o la stringa S1 non esista in T
 (o entrambe), la funzione deve ritornare 0. Ad esempio verificaDiscendenza(T1, “Alice”,
 “John”) ritornerà 1, mentre verificaDiscendenza(T1, “Max”, “John”) ritornerà 0.*/
-/*
-int verificaDiscendenza_aux(BTree T, stringa S2){
-   /* if(T){
-        return strcmp(T->nome,S2) || verificaDiscendenza_aux(T->dx,S2) || verificaDiscendenza_aux(T->sx, S2);
-    }
-<<<<<<< HEAD
-*/
 
-=======
-*//*
->>>>>>> 77e1ca531aa9c2cce39d8e9e645fe0c9152ab4c7
+int verificaDiscendenza_aux(BTree T, stringa S2){
     if(T){
-        //stampaStringaArray(T->nome);
-        printf("\n");
+        return (strcmp(T->nome,S2)==0) || verificaDiscendenza_aux(T->dx,S2) || verificaDiscendenza_aux(T->sx, S2);
+    }
+/*
+    if(T){
         if(strcmp(T->nome,S2)==0){
-            printf("%s = %s\n",T->nome,S2);
             return 1;
         }
         return verificaDiscendenza_aux(T->sx,S2) || verificaDiscendenza_aux(T->dx,S2);
-    }
-    return 0;
-}
-<<<<<<< HEAD
-
-int trova_nodo(BTree a1, stringa n){
-    if(a1){
-        if(strcmp(a1->nome,n)==0){
-            printf("%s = %s\n",a1->nome,n);
-            return 1;
-        }else{
-            if(n<a1->nome){
-                return trova_nodo(a1->sx,n);
-            }else{
-                return trova_nodo(a1->dx,n);
-            }
-        }
-    }
+    }*/
     return 0;
 }
 
-=======
-*/
-/*
->>>>>>> 77e1ca531aa9c2cce39d8e9e645fe0c9152ab4c7
 int verificaDiscendenza(BTree T, stringa S1, stringa S2){
     if(T){
 
         if(strcmp(T->nome,S1)==0){
-            printf("%s = %s\n",T->nome,S1);
             return verificaDiscendenza_aux(T,S2);
-            //return trova_nodo(T,S2);
         }
         return verificaDiscendenza(T->sx,S1,S2) || verificaDiscendenza(T->dx,S1,S2);
     }
     return 0;
 }
+
 /*
 int verificaDiscendenza(BTree T, stringa S1, stringa S2){
     if(T){
@@ -83,7 +53,7 @@ int verificaDiscendenza(BTree T, stringa S1, stringa S2){
     }
     return 0;
 }
-*/
+
 
 int verificaDiscendenza(BTree T, stringa a){
     if(T==NULL)
@@ -112,7 +82,7 @@ int funzionaCheFunziona(BTree T, stringa a, stringa b){
     return funzionaCheFunziona(T->sx,a,b) || funzionaCheFunziona(T->dx,a,b);
 
 }
-
+*/
 BTree crea_albero_discendenza(){
     BTree T=(BTree)malloc(sizeof(nodo_albero));
     BTree root=T;
@@ -153,12 +123,12 @@ BTree crea_albero_discendenza(){
 void esame_giugno2016(){
     BTree T=crea_albero_discendenza();
     //printf("%d\n",verificaDiscendenza_aux(T,"john"));
-    if(funzionaCheFunziona(T,"alice","john")){
+    if(verificaDiscendenza(T,"alice","john")){
         printf("john discende da alice\n");
     }else{
         printf("john non discende da alice\n");
     }
-    if(funzionaCheFunziona(T,"max","john")){
+    if(verificaDiscendenza(T,"max","john")){
         printf("john discende da max\n");
     }else{
         printf("john non discende da max\n");
